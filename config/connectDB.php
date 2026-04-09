aa7487616bbcce3ef64e7f9710b4b81e55f3d77e
testusermail44-cmyk <testusermail44@gmail.com>
2026-04-09 21:55:16 +0300

    Update connectDB.php

--- config/connectDB.php
+++ config/connectDB.php
@@ -1,7 +1,7 @@
 <?php
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=goncharnya;charset=utf8mb4',
-    'root',
-    '',
+    'mysql:host=sql7.freesqldatabase.com;dbname=sql7822751;charset=utf8mb4',
+    'sql7822751',
+    'v2hGuqBJpl',   
     [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ]);
 ?>
