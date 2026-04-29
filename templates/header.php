94a9f24006bcb98de540d59eff5909a5ff523dc6
testusermail44-cmyk <testusermail44@gmail.com>
2026-04-29 23:01:26 +0300

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,5 @@
 <?php
+include('../models/user.php')
 if (!isset($_SESSION['pottery_user']) && isset($_COOKIE['remember_user'])) {
     $userId = $_COOKIE['remember_user'];
     $user = getUserById($pdo, $userId);
